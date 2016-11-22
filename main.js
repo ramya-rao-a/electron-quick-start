d684cd9bc736deb681601d713816235da1359137
Ramya Achutha Rao <ramyar@microsoft.com>
2016-11-21 16:37:38 -0800

    Different submit url and metadata for main and renderer process

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const electron = require('electron')
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
-
+const crashReporter = electron.crashReporter
 const path = require('path')
 const url = require('url')
 
@@ -22,9 +22,6 @@ function createWindow () {
     slashes: true
   }))
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
@@ -37,7 +34,15 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+    crashReporter.start({
+      productName: 'dummy-product-main',
+      companyName: 'dummy-company-main',
+      submitURL: 'http://localhost:8080/uploadDump/mainDump'
+    });
+
+    createWindow();
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
--- renderer.js
+++ renderer.js
@@ -1,3 +1,24 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const crashReporter = require('electron').crashReporter
+
+
+var button1 = document.createElement('button');
+button1.textContent = 'Initialize Crash Reporter';
+document.body.appendChild(button1);
+button1.addEventListener('click', () => {
+   crashReporter.start({
+    productName: 'dummy-product-renderer',
+    companyName: 'dummy-company-renderer',
+    submitURL: 'http://localhost:8080/uploadDump/rendererDump'
+  });
+})
+
+var button2 = document.createElement('button');
+button2.textContent = 'Crash in the current Renderer process';
+document.body.appendChild(button2);
+button2.addEventListener('click', () => {
+    process.crash();
+})
